4a1d3f67f743680fb6ea740955fc183512d0b02b
Alexey Napalkov <napalkov-a@mail.ru>
2021-08-15 22:15:38 +0300

    вавававава

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'UI/VerticalList.dart';
 
-void main()=>runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-  color: Colors.blue,
-  home: Scaffold(
-    body: VerticalList(),
-  ),
-));
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      color: Colors.blue,
+      home: Scaffold(
+        body: VerticalList(),
+      ),
+    ));
+// давайте проверим токены
